ac69f5fb6166d9b16f45d40e5e1a4c7cf27b1a2f
Vadim Volkov <Srvdev.volkov@globus.ru>
2022-04-09 00:47:58 +0300

    Added RomanToInteger solved

---
+++ src/Leetcode.Solution.Test/Leetcode.Test/LeetcodeTest.cs
@@ -0,0 +1,22 @@
+using Leetcode.Solution.Algorithms;
+using Xunit;
+
+namespace TestProject1;
+
+public class LeetcodeTest
+{
+    private RomanToInteger? _sut;
+
+    [Theory]
+    [InlineData("LVIII", 58)]
+    [InlineData("MCMXCIV", 1994)]
+    public void RomanToInt_Return_Valid_Integer(string roman, int expected)
+    {
+        // Arrange
+        _sut = new();
+        // Act
+        int result = _sut.RomanToInt(roman);
+        // Assert
+        Assert.Equal(expected,result);
+    }
+}
