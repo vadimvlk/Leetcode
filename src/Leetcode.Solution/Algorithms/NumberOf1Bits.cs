8c0f618ace2e2f9a78e863a6df17f14b8d29816f
Vadim Volkov <vadimvlk0705@gmail.com>
2022-04-09 10:49:28 +0300

    Added NumberOf1Bits solved

---
+++ src/Leetcode.Solution/Algorithms/NumberOf1Bits.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode.Solution.Algorithms;
+
+public class NumberOf1Bits
+{
+   //
+    // Note:
+    //
+    // Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+    //     In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+    // Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+    public int HammingWeight(uint n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            count++;
+            n = n & (n - 1);
+        }
+
+        return count;
+    }
+}
