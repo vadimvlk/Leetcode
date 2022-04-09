8f3714e3a6f48a7da4454d2f3e6c77126d0d9707
Vadim Volkov <vadimvlk0705@gmail.com>
2022-04-09 10:49:28 +0300

    Added FindBottomLeftTreeValue resolve

---
+++ src/Leetcode.Solution/Algorithms/FindBottomLeftTreeValue.cs
@@ -0,0 +1,59 @@
+﻿namespace Leetcode.Solution.Algorithms;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class FindBottomLeftTreeValue
+{
+    // Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+    // Example 1:
+    // Input: root = [2,1,3]
+    // Output: 1
+    public int FindBottomLeftValue(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        var result = 0;
+        while (queue.Count > 0)
+        {
+            var size = queue.Count;
+            for (int i = 0; i < size; i++)
+            {
+                var node = queue.Dequeue();
+                if (i == 0)
+                {
+                    result = node.val;
+                }
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
