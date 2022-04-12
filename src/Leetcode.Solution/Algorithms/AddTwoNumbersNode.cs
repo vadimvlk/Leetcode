72f2c32ad96d072accde4bf3500e5e92e03a92d5
Vadim Volkov <vadimvlk0705@gmail.com>
2022-04-12 10:57:19 +0300

    Added AddTwoNumbersNode solved

---
+++ src/Leetcode.Solution/Algorithms/AddTwoNumbersNode.cs
@@ -0,0 +1,59 @@
+namespace Leetcode.Solution.Algorithms;
+
+public class AddTwoNumbersNode
+{
+    // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    //
+    //     You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    //
+    //
+    // Input: l1 = [2,4,3], l2 = [5,6,4]
+    // Output: [7,0,8]
+    // Explanation: 342 + 465 = 807.
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var result = new ListNode();
+        var current = result;
+        var carry = 0;
+
+        while (l1 != null || l2 != null)
+        {
+            var sum = carry;
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            current.next = new ListNode(sum);
+            current = current.next;
+        }
+
+        if (carry > 0)
+        {
+            current.next = new ListNode(carry);
+        }
+
+        return result.next;
+    }
+}
