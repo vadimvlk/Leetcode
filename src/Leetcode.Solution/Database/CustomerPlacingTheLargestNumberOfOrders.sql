1bf77a7e77def220f9902bdc219ce8a0305ea5a3
Vadim Volkov <vadimvlk0705@gmail.com>
2022-05-02 12:41:20 +0300

    Added CustomerPlacingTheLargestNumberOfOrders.sql

---
+++ src/Leetcode.Solution/Database/CustomerPlacingTheLargestNumberOfOrders.sql
@@ -0,0 +1,29 @@
+-- +-----------------+----------+
+-- | Column Name     | Type     |
+-- +-----------------+----------+
+-- | order_number    | int      |
+-- | customer_number | int      |
+-- +-----------------+----------+
+-- order_number is the primary key for this table.
+-- This table contains information about the order ID and the customer ID.
+--  
+-- 
+-- Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+-- 
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+-- 
+-- The query result format is in the following example.
+
+SELECT customer_number FROM
+    (
+        SELECT
+            customer_number,
+            COUNT(1) as orders
+        FROM
+            Orders
+        GROUP BY
+            customer_number
+    ) _
+ORDER BY
+    orders DESC
+LIMIT 1
